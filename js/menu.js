2d69796b7b13fa409da93bc3cb83841672ce5c27
Laura Brizuela <laura.brizuela93@gmail.com>
2021-03-29 11:50:38 -0300

    se añade pagina-gastronomia

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+/* var btnMenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+btnMenu.addEventListener('click',function(){
+   nav.classList.toggle('muestrate');
+}) */
+let = link;
+document.getElementById("facebook").onclick = function(){
+  link = document.getElementById("facebook").
+}
